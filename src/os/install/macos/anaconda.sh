74abdd4c583e3af3c499f54c533fd3cf7540e49d
eli knaap <ek@knaaptime.com>
2018-10-24 00:51:48 -0700

    execute anaconda w/ sudo

--- src/os/install/macos/anaconda.sh
+++ src/os/install/macos/anaconda.sh
@@ -11,7 +11,7 @@ main() {
     print_in_purple "\n   Anaconda\n\n"
 
     wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-MacOSX-x86_64.sh -O ~/anaconda.sh
-    chmod +x ~/anaconda.sh
+    sudo chmod +x ~/anaconda.sh
     bash ~/anaconda.sh -b -p $HOME/anaconda
 
 }
