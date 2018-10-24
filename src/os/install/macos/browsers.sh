876711379f04fb15b88d7d712a16358257ffdf8d
eli knaap <ek@knaaptime.com>
2018-10-23 22:19:31 -0700

    remove flash and safari preview

--- src/os/install/macos/browsers.sh
+++ src/os/install/macos/browsers.sh
@@ -19,18 +19,3 @@ printf "\n"
 
 brew_install "Firefox" "firefox" "caskroom/cask" "cask"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
-brew_install "Flash" "flash-npapi" "caskroom/cask" "cask"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
-# `Safari Technology Preview` requires macOS 10.11.4 or la
-# https://github.com/alrra/dotfiles/issues
-
-if is_supported_version "$(get_os_version)" "10.11.4"; then
-    printf "\n"
-    brew_install "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
-fi
