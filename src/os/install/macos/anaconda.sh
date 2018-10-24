d512601dc6326581cd461ff13061ac767b0f7335
eli knaap <ek@knaaptime.com>
2018-10-24 01:03:10 -0700

    different chmod for anaconda script

--- src/os/install/macos/anaconda.sh
+++ src/os/install/macos/anaconda.sh
@@ -11,7 +11,7 @@ main() {
     print_in_purple "\n   Anaconda\n\n"
 
     wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-MacOSX-x86_64.sh -O $HOME/Downloads/anaconda.sh
-    sudo chmod +x $HOME/Downloads/anaconda.sh
+    sudo chmod 755 $HOME/Downloads/anaconda.sh
     bash $HOME/Downloads/anaconda.sh -b -p $HOME/anaconda
 
 }
