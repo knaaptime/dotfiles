59917a4cd02d19bce5c6c47bc2a6db3c54dc2f3a
eli knaap <ek@knaaptime.com>
2018-10-24 01:15:06 -0700

    try using execute cmd

--- src/os/install/macos/anaconda.sh
+++ src/os/install/macos/anaconda.sh
@@ -10,9 +10,8 @@ main() {
 
     print_in_purple "\n   Anaconda\n\n"
 
-    wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-MacOSX-x86_64.sh -O $HOME/Downloads/anaconda.sh
-    sudo chmod 755 $HOME/Downloads/anaconda.sh
-    bash $HOME/Downloads/anaconda.sh -b -p $HOME/anaconda
+    execute "wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-MacOSX-x86_64.sh -O $HOME/Downloads/anaconda.sh"
+    execute "sudo chmod 755 $HOME/Downloads/anaconda.sh && bash $HOME/Downloads/anaconda.sh -b -p $HOME/anaconda"
 
 }
 
